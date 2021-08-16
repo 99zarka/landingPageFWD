3808b3526116910e6fa286597ea02017132f963a
99zarka <53001011+99zarka@users.noreply.github.com>
2021-08-16 23:41:22 +0200

    Feat: Navigation Menu-Smooth Scrolling-Scroll Bar
    1- Added JavaScript code to generate The navigation menu depending on
    the heading of each section.
    2- Each item in the menu stores the ID of the corresponding section to
    scroll to this section after clicking the item.
    3- Hiding or showing a  scroll bar to the navigation menu according to
    the size of the screen.
    This feature will be crucial for phone users.
    4- Changed some properties of the CSS styling related to the
    navigation menu in order to make it easier to calculate
    its width.

--- js/app.js
+++ js/app.js
@@ -55,3 +55,46 @@
 // Set sections as active
 
 
+//Generating the navigation bar & adding it to the html file
+const fragment = document.createDocumentFragment(); 
+let navigationBar=document.getElementById("navbar__list");
+let allSections=document.getElementsByTagName("section");
+
+for(let i=0;i<allSections.length;i++){
+    let navItem = document.createElement('li');
+    navItem.textContent=allSections[i].querySelector("h2").textContent;
+    navItem.setAttribute("data-section-id",allSections[i].id);
+    navItem.id=`item${i+1}`;
+    allSections[i].setAttribute("data-navItem-id",navItem.id);
+    //saving the id of the section in its corresponding navigation item on the top menu
+    //so each item could be paired with its reffered section, and vice versa
+    //the nav item and the section are pointing at one another using IDs
+    fragment.appendChild(navItem);
+}
+
+navigationBar.appendChild(fragment);
+
+
+//scrolling functionality
+let scrollToSection=function (evt){
+    if(evt.target.nodeName === 'LI'){
+        let el=document.getElementById(evt.target.getAttribute("data-section-id"));
+        el.scrollIntoView({behavior: "smooth"});
+    }
+}
+
+navigationBar.addEventListener('click',scrollToSection);
+
+//Showing a horizontal scroll bar when the screen doesn't fit the navigation bar
+let ShowHideScrollBar=function(){
+    let pageWidth=document.querySelector("html").offsetWidth;
+    if(navigationBar.scrollWidth<=pageWidth)
+        navigationBar.style.overflowX="hidden";
+    else 
+        navigationBar.style.overflowX="scroll";
+}
+window.addEventListener('load', ShowHideScrollBar);
+//to style the navigation list depending on the size of the screen while initialing the web page
+window.addEventListener('resize', ShowHideScrollBar);
+
+
