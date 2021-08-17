17edcceea43078e2a6200329d6e496e8f90d703d
99zarka <53001011+99zarka@users.noreply.github.com>
2021-08-17 10:06:56 +0200

    Feat: Button (add sections)-Active Section-Style
    HTML:
    Added a new button at the end of the page.
    The button adds one more section by clicking it.
    That will make it easier for testing the features.
    
    JavaScript:
    1- Added a button that enables the user to insert dummy sections to
    make it easier to test the web page.
    2- Added a function to calculate if an element is visible
    on the screen.
    3- Added a function to update and determine which section is being
    viewed (active class), hence change some CSS style which makes
    the page prettier to navigate.
    
    
    CSS:
    Updated the CSS style to keep up with the features.
    The corresponding navigation item of visible section now is
    distinguished from other items and highlighted by the color blue.
    The heading of an active section is highlighted in yellow.

--- js/app.js
+++ js/app.js
@@ -60,18 +60,24 @@ const fragment = document.createDocumentFragment();
 let navigationBar=document.getElementById("navbar__list");
 let allSections=document.getElementsByTagName("section");
 
-for(let i=0;i<allSections.length;i++){
+let initializeNavItem= function(section,index){
     let navItem = document.createElement('li');
-    navItem.textContent=allSections[i].querySelector("h2").textContent;
-    navItem.setAttribute("data-section-id",allSections[i].id);
-    navItem.id=`item${i+1}`;
-    allSections[i].setAttribute("data-navItem-id",navItem.id);
+    navItem.textContent=section.querySelector("h2").textContent;
+    navItem.setAttribute("data-section-id",section.id);
+    navItem.id=`item${index+1}`;
+    section.setAttribute("data-navItem-id",navItem.id);
+
+    return navItem;
     //saving the id of the section in its corresponding navigation item on the top menu
     //so each item could be paired with its reffered section, and vice versa
     //the nav item and the section are pointing at one another using IDs
-    fragment.appendChild(navItem);
+
 }
 
+let i=0;
+for(i=0;i<allSections.length;i++){
+    fragment.appendChild(initializeNavItem(allSections[i],i));
+}
 navigationBar.appendChild(fragment);
 
 
@@ -98,3 +104,60 @@ window.addEventListener('load', ShowHideScrollBar);
 window.addEventListener('resize', ShowHideScrollBar);
 
 
+//a button for adding a dummy section to make it easier to test the web page
+let dummySection=(allSections)=>  `<section id="section${allSections.length+1}" data-nav="Section ${allSections.length+1}">
+<div class="landing__container">
+  <h2>Section ${allSections.length+1}</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+  <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+</div>
+</section>`;
+
+let button1=document.getElementById('button1');
+button1.addEventListener('click',function(){
+    allSections[allSections.length-1].insertAdjacentHTML('afterend',dummySection(allSections));
+    navigationBar.appendChild(initializeNavItem(allSections[i],i++));
+    ShowHideScrollBar();
+    navigationBar.lastElementChild.scrollIntoView({behavior: "smooth"});
+    button1.scrollIntoView({behavior: "smooth"});
+
+})
+
+//updating 'your-active-class' while scrolling by calculating if a section is visible on the screen 
+function isVisible(elem) {
+    bound=elem.getBoundingClientRect();
+    if(bound.top>window.innerHeight && bound.bottom>window.innerHeight) //when the element is fully above the viewport
+        return false;
+    if(bound.top<0 && bound.bottom<0) //when the element is fully below the viewport
+        return false;
+    
+    let mean=(bound.top+bound.bottom)/2;
+    if(mean>window.innerHeight || mean<0) //when the centre (middle base) of the element is not visible
+        return false; 
+
+    return true;
+  }
+
+navigationBar.firstChild.classList.add('your-active-nav-menu'); //initial value
+
+let changeActiveClass=function(elem){
+    let previousActiveElement=document.querySelector('.your-active-class');
+    previousActiveElement.classList.remove('your-active-class');
+    elem.classList.add('your-active-class');
+
+    let previousActiveNavItem=document.querySelector('.your-active-nav-menu');
+    previousActiveNavItem.classList.remove('your-active-nav-menu');
+
+    let newActiveNavItemID=elem.getAttribute('data-navItem-id');
+    let newActiveNavItem=document.getElementById(newActiveNavItemID);
+    newActiveNavItem.classList.add('your-active-nav-menu');
+    newActiveNavItem.scrollIntoView({behavior: "smooth", inline : "center"});
+}
+
+window.addEventListener('scroll', function() {
+    Array.from(allSections).forEach(function(elem){
+        if(isVisible(elem))
+            changeActiveClass(elem);
+    })
+})
