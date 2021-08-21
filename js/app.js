05bb43a978b05a72215cf895cd7f1959066596c8
99zarka <53001011+99zarka@users.noreply.github.com>
2021-08-21 17:59:18 +0200

    Refactor: Rearranging-Renaming-Comments-some Fixes
    1- Rearranged the code in the same order which was specified in the starter code.
    2- Renaming buttons, some functions and variables.
    3- Added more comments and rearranged them.
    4- Fixed the hiding navigation bar.
    If The user is dragging the navigation bar, it will always be shown.
    If The user is scrolling the navigation bar, it will always be shown.

--- js/app.js
+++ js/app.js
@@ -17,52 +17,27 @@
  * Define Global Variables
  * 
 */
-
+const fragment = document.createDocumentFragment(); 
+const navigationBar=document.getElementById("navbar__list");
+const allSections=document.getElementsByTagName("section");
+const pageHeader=document.querySelector('.page__header');
+const buttonToTop=document.querySelector('#buttonToTop');
+const buttonDummySection=document.getElementById('buttonDummySection');
+const buttonNewSection=document.getElementById('buttonNewSection');
+let oldMousePos=0
+let oldScrollPos=0 //Position of navigation menu's horizontal scroll bar
+let timer = 0;
+const timeLimit = 3000 //time in ms. navigation bar is hidden beyond this limit.
+const TimerInterval=100; //time in ms
+let mouseHoldOnNavBar=false;
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
-//Generating the navigation bar & adding it to the html file
-const fragment = document.createDocumentFragment(); 
-let navigationBar=document.getElementById("navbar__list");
-let allSections=document.getElementsByTagName("section");
-let pageHeader=document.querySelector('.page__header')
-
-let initializeNavItem= function(section,index){
-    let navItem = document.createElement('li');
+function initializeNavItem(section,index){
+    const navItem = document.createElement('li');
     navItem.textContent=section.querySelector("h2").textContent;
     navItem.setAttribute("data-section-id",section.id);
     navItem.id=`item${index+1}`;
@@ -74,22 +49,59 @@ let initializeNavItem= function(section,index){
     //the nav item and the section are pointing at one another using IDs
 }
 
-for(let i=0;i<allSections.length;i++){
-    fragment.appendChild(initializeNavItem(allSections[i],i));
-}
-navigationBar.appendChild(fragment);
+//When the middle line of the screen touches the section, the section will be marked as visible.
+function isVisible(elem) {
+    const middleLineOfScreen=window.scrollY+window.innerHeight/2;
+    const elemBottom=elem.offsetTop+elem.offsetHeight;
+    if(elem.offsetTop<middleLineOfScreen && middleLineOfScreen<elemBottom )
+        return true;
+        
+    return false;
+  }
 
-//scrolling functionality
-let scrollToSection=function (evt){
-    if(evt.target.nodeName === 'LI'){
-        let el=document.getElementById(evt.target.getAttribute("data-section-id"));
-        window.scrollTo({top: el.offsetTop, behavior: 'smooth'} );
-    }
+//Showing the "Back to top" button when reaching the last section, otherwise it's going to be hidden
+function showHideBackToTopButton() {
+    if(allSections[allSections.length-1].className=='your-active-class' || isVisible(document.querySelector('footer')))
+        buttonToTop.style.display='inline';
+    else
+        buttonToTop.style.display='none';
 }
+
+function createDummySectionHTML(numOfNewSection){
+    return `
+    <section id="section${numOfNewSection}" data-nav="Section ${numOfNewSection}">
+        <div class="landing__container">
+            <h2>Section ${numOfNewSection}</h2>
+            <div class='paragraphs'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    
+                <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+            </div>
+        </div>
+    </section>`;
+    }
+
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+function initializeNavBar(){
+    for(let i=0;i<allSections.length;i++)
+        fragment.appendChild(initializeNavItem(allSections[i],i));
+    navigationBar.appendChild(fragment);
+}
+
 
 //Showing a horizontal scroll bar (and grabbing cursor) when the screen doesn't fit the navigation bar
-let ShowHideScrollBar=function(){
-    let pageWidth=document.querySelector("html").offsetWidth;
+function ShowHideNavBarScrollBar(){
+    const pageWidth=document.querySelector("html").offsetWidth;
     if(navigationBar.scrollWidth<=pageWidth){
             navigationBar.style.overflowX="hidden";
             pageHeader.style.cursor='default';
@@ -97,70 +109,40 @@ let ShowHideScrollBar=function(){
     else{
         navigationBar.style.overflowX="scroll";
         pageHeader.style.cursor='grab';
-    }
-        
+    }   
 }
-window.addEventListener('load', ShowHideScrollBar);
-//to style the navigation list depending on the size of the screen while initialing the web page
-window.addEventListener('resize', ShowHideScrollBar);
 
+// Add class 'active' to section when near top of viewport
 //updating 'your-active-class' while scrolling by calculating if a section is visible on the screen 
-//When the middle line of the screen touches the section, the section will be marked as visible.
-function isVisible(elem) {
-    let middleLineOfScreen=window.scrollY+window.innerHeight/2;
-    let elemBottom=elem.offsetTop+elem.offsetHeight;
-    if(elem.offsetTop<middleLineOfScreen && middleLineOfScreen<elemBottom )
-        return true;
-        
-    return false;
-  }
-
-navigationBar.firstChild.classList.add('your-active-nav-menu'); //initial value
-
-let changeActiveClass=function(elem){
-    let previousActiveElement=document.querySelector('.your-active-class');
+function changeActiveClass(elem){
+    const previousActiveElement=document.querySelector('.your-active-class');
     previousActiveElement.classList.remove('your-active-class');
     elem.classList.add('your-active-class');
 
-    let previousActiveNavItem=document.querySelector('.your-active-nav-menu');
+    const previousActiveNavItem=document.querySelector('.your-active-nav-menu');
     previousActiveNavItem.classList.remove('your-active-nav-menu');
 
-    let newActiveNavItemID=elem.getAttribute('data-navItem-id');
-    let newActiveNavItem=document.getElementById(newActiveNavItemID);
+    const newActiveNavItemID=elem.getAttribute('data-navItem-id');
+    const newActiveNavItem=document.getElementById(newActiveNavItemID);
     newActiveNavItem.classList.add('your-active-nav-menu');
     navigationBar.scrollTo({ //centering the active item in the middle of the navigation bar.
         left: newActiveNavItem.offsetLeft-window.innerWidth*0.5+newActiveNavItem.offsetWidth*0.5+2, //2 is the half of the margin (4px)
         behavior: 'smooth'});
 }
 
-window.addEventListener('scroll', function() {
-    Array.from(allSections).forEach(function(elem){
-        if(isVisible(elem))
-            changeActiveClass(elem);
-    })
-})
+// Scroll to anchor ID using scrollTO event smoothly
+function scrollToSection(evt){
+    if(evt.target.nodeName === 'LI'){
+        const el=document.getElementById(evt.target.getAttribute("data-section-id"));
+        window.scrollTo({top: el.offsetTop, behavior: 'smooth'} );
+    }
+}
 
-//Back to top button
-
-let buttonToTop=document.querySelector('#buttonToTop');
-buttonToTop.addEventListener('click',function(){
-    window.scrollTo({top: 0, behavior: 'smooth'});
-})
-
-//showing the "Back to top" button when reaching the last section, otherwise it's going to be hidden
-window.addEventListener('scroll', function() {
-    if(allSections[allSections.length-1].className=='your-active-class' || isVisible(document.querySelector('footer')))
-        buttonToTop.style.display='inline';
-    else
-        buttonToTop.style.display='none';
-})
-
-//a button for adding a dummy section to make it easier to test the web page
-
-let addNewSection=function(htmlText){
+//A button for adding a dummy section to make it easier to test the web page
+function addNewSection(htmlText){
     allSections[allSections.length-1].insertAdjacentHTML('afterend',htmlText);
     navigationBar.appendChild(initializeNavItem(allSections[allSections.length-1],allSections.length-1));
-    ShowHideScrollBar();
+    ShowHideNavBarScrollBar();
     navigationBar.scrollTo({
         left: navigationBar.scrollWidth ,
         behavior: 'smooth'});
@@ -168,42 +150,23 @@ let addNewSection=function(htmlText){
     changeActiveClass(allSections[allSections.length-1])
 }
 
-let dummySectionHTML=(numOfNewSection)=>
-`<section id="section${numOfNewSection}" data-nav="Section ${numOfNewSection}">
-    <div class="landing__container">
-        <h2>Section ${numOfNewSection}</h2>
-        <div class='paragraphs'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-
-            <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-        </div>
-    </div>
-</section>`;
-
-let button1=document.getElementById('button1');
-button1.addEventListener('click',function(){
-    addNewSection(dummySectionHTML(allSections.length+1));
-    window.scrollBy({
-        top: allSections[allSections.length-1].offsetHeight,
-        behavior: 'smooth'});
-})
-
 //Feature: adding a new section entered by the user.
-let button2=document.getElementById('button2');
-button2.addEventListener('click',function(){
-    let newTitle=document.getElementById('newTitle').value.trim();
+
+function addNewSectionByUser(){
+    const newTitle=document.getElementById('newTitle').value.trim();
     let newText=document.getElementById('newText').value.trim();
     while(newText.search('\n\n')>=0)
         newText=newText.replaceAll('\n\n','\n'); //to discard empty newlines.
 
     newText=newText.replaceAll('\n','<p>');
 
-    let htmlText=`<section id="section${allSections.length+1}" data-nav="Section ${allSections.length+1}">
-                    <div class="landing__container">
-                        <h2>${newTitle}</h2>
-                        <div class='paragraphs'>${newText}</div>
-                    </div>
-                  </section>`;
+    const htmlText=`
+    <section id="section${allSections.length+1}" data-nav="Section ${allSections.length+1}">
+        <div class="landing__container">
+            <h2>${newTitle}</h2>
+            <div class='paragraphs'>${newText}</div>
+        </div>
+    </section>`;
 
     if(newTitle&& newText){
         addNewSection(htmlText);
@@ -216,31 +179,25 @@ button2.addEventListener('click',function(){
     else{
         alert('You must fill Title and Text fields.');
     }
-})
+}
 
 //Section collapse feature
-document.addEventListener('click',function(evt){
-    if(evt.target.nodeName === 'H2'){
-        
-        classListArray=Array.from(evt.target.classList);
-        if(!classListArray.includes('collapsed')){
-            evt.target.parentElement.parentElement.style.minHeight='0';
-            evt.target.nextElementSibling.style.maxHeight='0';
-        }
-        else{
-            evt.target.nextElementSibling.style.maxHeight='100%';
-            evt.target.parentElement.parentElement.style.minHeight='80vh';
-        }
-        evt.target.classList.toggle('collapsed');
+function collapseSection(evt){
+    classListArray=Array.from(evt.target.classList);
+    if(!classListArray.includes('collapsed')){
+        evt.target.parentElement.parentElement.style.minHeight='0';
+        evt.target.nextElementSibling.style.maxHeight='0';
     }
-})
+    else{
+        evt.target.nextElementSibling.style.maxHeight='100%';
+        evt.target.parentElement.parentElement.style.minHeight='80vh';
+    }
+    evt.target.classList.toggle('collapsed');
+}
 
 //Scroll navigation menu by clicking & dragging it
-
-let oldMousePos=0
-let oldScrollPos=0
-
 function dragScrolling(evt){
+    mouseHoldOnNavBar=true;
     if(pageHeader.style.cursor=='grab')
         pageHeader.style.cursor='grabbing';
     oldScrollPos=navigationBar.scrollLeft;
@@ -251,45 +208,95 @@ function dragScrolling(evt){
     }
 
     function abortDragScrolling(evt){
-        ShowHideScrollBar();
+        mouseHoldOnNavBar=false;
+        timer=0;
+        ShowHideNavBarScrollBar();
         document.removeEventListener('mousemove',moveScrollBar);
         document.removeEventListener('mouseup',abortDragScrolling);
         if(evt.pageX!=oldMousePos) //clicking event will be aborted if the curser moves
             navigationBar.removeEventListener('click',scrollToSection); //solves the conflict between the dragging and clicking
-        }
+    }
 
     document.addEventListener('mousemove', moveScrollBar);
     document.addEventListener('mouseup', abortDragScrolling);
     navigationBar.addEventListener('click',scrollToSection);
 }
 
-navigationBar.addEventListener('mousedown',dragScrolling);
-
-//hide navigation bar while not scrolling
-
-let timer = 0;
-const timeLimit = 3000 //time in ms
-//const show = new Event('timeUp');
-setInterval(() => {
+//When the timer exceeds a specfic time, the navigation bar will be hidden.
+function continueOrResetTimer()  {
     timer+=100;
+
     //If The user is still on top of the page, the navigation bar will be shown
     if(window.scrollY<allSections[0].offsetTop) 
         timer=0;
 
+    //If The user is dragging the navigation bar, it will always be shown
+    if(mouseHoldOnNavBar)
+        timer=-Infinity; 
+
     if(timer>=timeLimit){
         navigationBar.style.maxHeight='0';
-        timer=timeLimit+100
+        timer=timeLimit+TimerInterval;
     }else
         navigationBar.style.maxHeight='20vh';
-}, 100);
+    
+}
 
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
 
-window.addEventListener('scroll',function(){
-    timer=0;
+// Build menu 
+//Generating the navigation bar & adding it to the html file
+initializeNavBar();
+
+//to style the navigation list depending on the size of the screen after initialing the web page or resizing
+window.addEventListener('load', ShowHideNavBarScrollBar);
+window.addEventListener('resize', ShowHideNavBarScrollBar);
+
+// Scroll to section on link click
+navigationBar.addEventListener('mousedown',dragScrolling);
+
+// Set sections as active
+navigationBar.firstChild.classList.add('your-active-nav-menu'); //initial value
+window.addEventListener('scroll', function() {
+    Array.from(allSections).forEach(function(elem){
+        if(isVisible(elem))
+            changeActiveClass(elem);
+    })
 })
 
+/* Back to top button */
+buttonToTop.addEventListener('click',function(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+})
+window.addEventListener('scroll', showHideBackToTopButton)
+
+
+/* Updating page content */
+
+buttonDummySection.addEventListener('click',function(){
+    addNewSection(createDummySectionHTML(allSections.length+1));
+    window.scrollBy({
+        top: allSections[allSections.length-1].offsetHeight,
+        behavior: 'smooth'});
+})
+
+buttonNewSection.addEventListener('click',addNewSectionByUser);
+
+document.addEventListener('click',function (evt){
+    if(evt.target.nodeName === 'H2')
+        collapseSection(evt);
+});
+
+/* Hide navigation bar while not scrolling */
+setInterval(continueOrResetTimer, TimerInterval);
+window.addEventListener('scroll',function(){ timer=0; });
 //Show the navigation bar if the curser touches the top 10% of the window
 window.addEventListener('mousemove',function(evt){
     if(evt.clientY<window.innerHeight/10)
         timer=0
-})
+});
+navigationBar.addEventListener('scroll',function(){ timer=0; });
